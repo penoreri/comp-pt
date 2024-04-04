f900d98efa630c2199811469ade8fc8ca0a9e77e
penoreri <142084168+penoreri@users.noreply.github.com>
2024-04-04 22:45:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print ("hi")
+import random
+import time
+from tkinter import *
+
+game_open = Tk()
+game_open.geometry("1920x1080")
+
+animal_options = ['Dog','Cat','Lion','Tiger','Elephant',
+                  'Zebra','Parrot','Panda','Chicken','Duck','Turkey']
+chosen_word = ""
+
+def scramble_word(word):
+    letters = list(word)
+    random.shuffle(letters)
+    return ''.join(letters)
+
+def new_game():
+    global chosen_word
+    chosen_word = random.choice(animal_options)
+    lbl_word.config(text=scramble_word)
+    lbl_result.config(text='')
+
+def check_word():
+    guessed_word = entry_guess.get().lower()
+
+game_open.mainloop()
